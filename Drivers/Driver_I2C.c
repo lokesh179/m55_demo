dc8047b6adcaac4f74d5c097605026094af9dfd1
lokesh179 <65767368+lokesh179@users.noreply.github.com>
2020-06-03 12:26:48 +0530

    Update Driver_I2C.c

--- Drivers/Driver_I2C.c
+++ Drivers/Driver_I2C.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2020 Arm Limited. All rights reserved........
- *
+ * *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the License); you may
